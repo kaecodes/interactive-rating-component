f4595022737e6d70230a7b890394128acc61a61a
Kaela Nguyen <kaedtn96@gmail.com>
2023-01-10 22:07:17 -0800

    finish basic functionality with JS

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ ratingNum.forEach((num) => {
   num.addEventListener('click', () => {
     num.style.backgroundColor = 'var(--orange)'; 
     num.style.color = 'var(--white)'; 
+
+    const ratingValue = num.innerHTML; 
+
+    console.log(ratingValue); 
+
+    submitBtn.addEventListener('click', () => {
+      document.getElementById('selected-rating').innerHTML = ratingValue; 
+      ratingCard.style.zIndex = '0'; 
+      thanksCard.style.zIndex = '1'; 
+    })
   })
 })
 
-submitBtn.addEventListener('click', () => {
-  ratingCard.style.zIndex = '0'; 
-  thanksCard.style.zIndex = '1'; 
-})
+
  
